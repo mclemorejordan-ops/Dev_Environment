39b2e4386163734ebccd5f87741adf53b633c9ae
mclemorejordan-ops <mclemore.jordan@gmail.com>
2026-03-04 04:25:12 -0500

    Revert to Dupe. Date

--- app/app.js
+++ app/app.js
@@ -2981,19 +2981,7 @@ function openExerciseHistoryModal(type, exerciseId, exNameOverride=null){
       head,
       list,
       el("div", { style:"height:10px" }),
-      el("div", { class:"btnrow" }, [
-  el("button", {
-    class:"btn",
-    onClick: () => {
-      Modal.close(); // go back to Workout modal
-    }
-  }, ["Back"]),
-
-  el("button", {
-    class:"btn",
-    onClick: Modal.close
-  }, ["Close"])
-])
+      el("button", { class:"btn", onClick: Modal.close }, ["Close"])
     ])
   });
 
